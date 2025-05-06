57e95547ff3fc1e7c7c70dccc44aa607a24d4e26
VinceeCarterr <220711806@students.uajy.ac.id>
2025-05-06 16:15:16 +0700

    Login Register
    - api integration
    - middleware
    - role based login and register

--- ReuseMart-laravel/app/Models/User.php
+++ ReuseMart-laravel/app/Models/User.php
@@ -2,15 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Models
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens, Notifiable;
 
     protected $table = 'user';
     protected $primaryKey = 'id_user';
+    public $timestamps = false;
     protected $fillable = [
         'id_role',
         'first_name',
@@ -25,9 +28,11 @@ class User extends Models
         'saldo',
     ];
 
+    protected $hidden = ['password', 'remember_token'];
+
     public function role()
     {
-        return $this->belongsTo(Role::class, 'id_role');
+        return $this->belongsTo(Role::class, 'id_role', 'id_role');
     }
 
     public function alamat()
